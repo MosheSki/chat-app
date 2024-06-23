a3968f962d5437b27a67f2b572e8949c3b3a3216
MosheSki <MoshLago@gmail.com>
2024-06-24 00:33:33 +0300

    build and deployment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -12,6 +13,8 @@ import connectDB from "./db/connectDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -28,11 +31,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: "Server Running on " + PORT,
-//   });
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 connectDB()
   .then(() => {
