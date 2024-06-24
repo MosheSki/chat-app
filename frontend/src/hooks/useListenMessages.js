2a9f318fe1579ee6e6a121515f70d217916731c5
MosheSki <MoshLago@gmail.com>
2024-06-24 14:23:32 +0300

    fix

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,24 +1,24 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useSocketContext } from "../context/SocketContext";
-import { useEffect } from "react";
-import { setMessages } from "../redux/conversationReducer";
-import notificationSound from "../assets/sounds/notification.mp3";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useSocketContext } from "../context/SocketContext";
+// import { useEffect } from "react";
+// import { setMessages } from "../redux/conversationReducer";
+// import notificationSound from "../assets/sounds/notification.mp3";
 
-const useListenMessages = () => {
-  const { socket } = useSocketContext();
-  const messages = useSelector((store) => store.conversation.messages);
-  const dispatch = useDispatch();
+// const useListenMessages = () => {
+//   const { socket } = useSocketContext();
+//   const messages = useSelector((store) => store.conversation.messages);
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    socket?.on("newMessage", (newMessage) => {
-      newMessage.shouldShake = true;
-      const sound = new Audio(notificationSound);
-      sound.play();
-      dispatch(setMessages([...messages, newMessage]));
-    });
+//   useEffect(() => {
+//     socket?.on("newMessage", (newMessage) => {
+//       newMessage.shouldShake = true;
+//       const sound = new Audio(notificationSound);
+//       sound.play();
+//       dispatch(setMessages([...messages, newMessage]));
+//     });
 
-    return () => socket?.off("newMessage");
-  }, [socket, dispatch, messages]);
-};
+//     return () => socket?.off("newMessage");
+//   }, [socket, dispatch, messages]);
+// };
 
-export default useListenMessages;
+// export default useListenMessages;
