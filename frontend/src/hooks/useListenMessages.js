8de21baee2f48aa2c1217b8846ecf4dc0ff43808
MosheSki <MoshLago@gmail.com>
2024-06-24 14:36:07 +0300

    fix

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,24 +1,27 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { useSocketContext } from "../context/SocketContext";
-// import { useEffect } from "react";
-// import { setMessages } from "../redux/conversationReducer";
-// import notificationSound from "../assets/sounds/notification.mp3";
+import { useDispatch, useSelector } from "react-redux";
+import { useSocketContext } from "../context/SocketContext";
+import { useEffect } from "react";
+import { setMessages } from "../redux/conversationReducer";
+import notificationSound from "../assets/sounds/notification.mp3";
 
-// const useListenMessages = () => {
-//   const { socket } = useSocketContext();
-//   const messages = useSelector((store) => store.conversation.messages);
-//   const dispatch = useDispatch();
+const useListenMessages = () => {
+  const { socket } = useSocketContext();
+  const { messages, selectedConversation } = useSelector(
+    (store) => store.conversation
+  );
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     socket?.on("newMessage", (newMessage) => {
-//       newMessage.shouldShake = true;
-//       const sound = new Audio(notificationSound);
-//       sound.play();
-//       dispatch(setMessages([...messages, newMessage]));
-//     });
+  useEffect(() => {
+    if (selectedConversation) {
+      socket?.on("newMessage", (newMessage) => {
+        newMessage.shouldShake = true;
+        const sound = new Audio(notificationSound);
+        sound.play();
+        dispatch(setMessages([...messages, newMessage]));
+      });
+    }
+    return () => socket?.off("newMessage");
+  }, [socket, dispatch, messages, selectedConversation]);
+};
 
-//     return () => socket?.off("newMessage");
-//   }, [socket, dispatch, messages]);
-// };
-
-// export default useListenMessages;
+export default useListenMessages;
