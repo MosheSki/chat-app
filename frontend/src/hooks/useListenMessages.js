eae1bbc918a32f23ae7bfc4aeb00bba2f824436d
MosheSki <MoshLago@gmail.com>
2024-06-24 15:53:38 +0300

    fix

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -13,9 +13,9 @@ const useListenMessages = () => {
 
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
+      console.log("new message conversation id:" + newMessage.conversationId);
+      console.log("conversation id:" + selectedConversation._id);
       if (newMessage.conversationId === selectedConversation._id) {
-        console.log("new message conversation id:" + newMessage.conversationId);
-        console.log("conversation id:" + selectedConversation._id);
         newMessage.shouldShake = true;
         const sound = new Audio(notificationSound);
         sound.play();
